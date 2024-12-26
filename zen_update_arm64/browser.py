3c0acb1b0c06e98211a82471076ff98ca9ef0643
kjfreidhof <kjfreidhof@home.com>
2024-12-26 11:24:33 -0500

    adding zen update for arm64

---
+++ zen_update_arm64/browser.py
@@ -0,0 +1,10 @@
+import wget 
+
+def main():
+     browser_url = "https://github.com/zen-browser/desktop/releases/download/1.0.2-b.5/zen.linux-aarch64.tar.bz2"
+     # browser_url = "https://github.com/zen-browser/desktop/releases/download/1.0.2-b.3/zen.linux-specific.tar.bz2"
+     file = wget.download(browser_url)
+
+main()
+
+
